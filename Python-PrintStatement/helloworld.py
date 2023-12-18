2d2f2dc06b11c567ad592930264d1b297065980d
Mickeydoe <michaeldanquah615@gmail.com>
2023-12-18 23:12:26 +0000

    New additions

--- Python-PrintStatement/helloworld.py
+++ Python-PrintStatement/helloworld.py
@@ -1,1 +1,14 @@
 print("Hello world")
+print()
+print("Blank line printed")
+
+print("Adding numbers")
+x = 42 + 206
+print(x)
+
+
+#Variables
+
+name = input("Whats is your name: ")
+print("Hello", name)
+
