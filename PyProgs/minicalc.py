b410848055b79f6cb33a4cbd59cae225236f580c
Mickeydoe <michaeldanquah615@gmail.com>
2023-12-19 13:27:46 +0000

    Calculator

--- PyProgs/minicalc.py
+++ PyProgs/minicalc.py
@@ -1,1 +1,19 @@
-#Welcome to my mini calculator
+print("Welcome to my mini calculator app")
+
+print("Enter the numbers you would like to add and subtract\n")
+
+print("ADDITION'N")
+firstnum = int(input("Enter the first number: "))
+secondnum = int(input("Enter the second number: "))
+
+add = firstnum + secondnum
+print(add)    
+print()  
+
+print("Subtraction\n")
+firstnum = int(input("Enter first number: "))
+secondnum = int(input("Enter second number: "))
+
+subtract = firstnum - secondnum
+print(subtract)
+
