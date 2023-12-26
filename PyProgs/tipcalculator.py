bc8508a518f1561b55c85d6dc2abd1023816b7e1
Mickeydoe <michaeldanquah615@gmail.com>
2023-12-26 16:29:11 +0000

    Changed naming conventions

--- PyProgs/tipcalculator.py
+++ PyProgs/tipcalculator.py
@@ -6,8 +6,8 @@ print(f"The amount you are paying is {charge}")
 
 tip_calc = 18/100 * charge
 sales_tax = 7/100 * charge
-total = charge + tipcalc + salestax
+total = charge + tip_calc + sales_tax
 
 print(f"Tip = {charge}")
-print(f"Sales tax = {salestax}")
+print(f"Sales tax = {sales_tax}")
 print(f"Grand total = {total}")
