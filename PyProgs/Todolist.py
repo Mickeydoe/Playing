a570187a151f29e6d8d0b31bbba6ff36c6b60f55
Mickeydoe <michaeldanquah615@gmail.com>
2023-12-30 20:28:12 +0000

    New commits

---
+++ PyProgs/Todolist.py
@@ -0,0 +1,38 @@
+#A TO-DO LIST APPLICATION
+
+print("Welcome to the TO-DO LIST Application")
+
+tasks = []
+
+
+while True:
+    
+    print("OPTIONS")
+    print("1. Add Task")
+    print("2. Display Task")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    
+    choice = input("What would you like to do?: ")
+    
+    if choice == "1":
+        print("Enter your task")
+        task = input("Add task: ")
+        tasks.append(task)
+    
+    elif choice == "2":
+        print("Displaying Tasks: ")
+        for task in tasks:
+            print(task)
+    
+    elif choice == "3":
+        rem_task = input("Enter the task you want to remove?: ")
+        tasks.remove(rem_task)
+    
+    elif choice == "4":
+        print("Quitting.....")
+        break
+    
+    else:
+        print("Invalid Option....")
--- PyProgs/weekth3project.py
+++ PyProgs/weekth3project.py
@@ -21,20 +21,28 @@ print(total)
 
 print()
 
+#total price average
 total_price_avg = total / len(prices)
 print(f"The total price average is {total_price_avg}")
 
 print()
-
+#new price list that reduces old prices by 5$
 red_prices = [prices - 5 for prices in prices]
 print(f"The new price list reduced by 5 is {red_prices}\n")
 
+print()
+
+#total revenue generated from products
 total_revenue = sum(quantity * price for quantity, price in zip(last_week, prices))
 print(f"The total revenue is {total_revenue}\n")
 
+print()
+
+#average daily revenue
 avg_daily_revenue = sum(last_week) / 7
 print(f"The daily average is {avg_daily_revenue}\n")
 
+#affordable pricing
 affordable_price = [product for product, price in zip(products, red_prices) if price < 30]
 print(f"The products that are less than 30$ are: {affordable_price}")
 
