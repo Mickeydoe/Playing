17746f9632653219bea1e22f445a04f8c776a153
Mickeydoe <michaeldanquah615@gmail.com>
2024-02-16 09:57:32 +0000

    Started file manager Script

---
+++ PyProgs/Filemanager.py
@@ -0,0 +1,15 @@
+print("Welcome to the File manager")
+print("Choose from the options Below")
+
+print("1. Create a new directory")
+print("2. Delete an existing directory")
+print("3. List the contents of a directory")
+print("4. Change to a directory")
+print("5. Create a new file")
+print("6. Write to an existing file")
+print("7. Read from an existing file")
+print("8.Delete an existing file")
+print("9. Exit")
+
+choice = input("Select an option: ........")
+
